0406fa72ad63ad9e0c5e0551261195a2ba349896
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-09-24 14:37:37 +0200

    Add support for AWS provider version 6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,6 @@
 import logging
 
-import boto3
-import pytest
 from infrahouse_core.logging import setup_logging
-
-# "303467602807" is our test account
-TEST_ACCOUNT = "303467602807"
-# TEST_ROLE_ARN = "arn:aws:iam::303467602807:role/state-bucket-tester"
-DEFAULT_PROGRESS_INTERVAL = 10
-TRACE_TERRAFORM = False
-
 
 LOG = logging.getLogger(__name__)
 setup_logging(LOG, debug=True)
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,49 +1,74 @@
-import sys
 from pprint import pprint
-from subprocess import CalledProcessError
 from os import path as osp
 from textwrap import dedent
+import os
 
+import pytest
 from pytest_infrahouse import terraform_apply
 
-from tests.conftest import TRACE_TERRAFORM, LOG
 
-
+@pytest.mark.parametrize("aws_provider_version", ["~> 5.31", "~> 6.0"])
 def test_state_bucket(
     keep_after,
     aws_region,
     test_role_arn,
+    aws_provider_version,
 ):
     terraform_module_dir = "test_data/state-bucket"
+
+    # Clean up any existing Terraform state files
+    for artifact_file in [".terraform.lock.hcl"]:
+        state_path = osp.join(terraform_module_dir, artifact_file)
+        try:
+            os.remove(state_path)
+        except FileNotFoundError:
+            pass
+
+    # Remove .terraform directory if it exists
+    terraform_dir = osp.join(terraform_module_dir, ".terraform")
     try:
-        with open(osp.join(terraform_module_dir, "terraform.tfvars"), "w") as fp:
+        import shutil
+
+        shutil.rmtree(terraform_dir)
+    except FileNotFoundError:
+        pass
+
+    # Update terraform.tf with the specified AWS provider version
+    with open(osp.join(terraform_module_dir, "terraform.tf"), "w") as fp:
+        fp.write(
+            dedent(
+                f"""
+                terraform {{
+                  required_providers {{
+                    aws = {{
+                      source  = "hashicorp/aws"
+                      version = "{aws_provider_version}"
+                    }}
+                  }}
+                }}
+                """
+            )
+        )
+
+    with open(osp.join(terraform_module_dir, "terraform.tfvars"), "w") as fp:
+        fp.write(
+            dedent(
+                f"""
+                region = "{aws_region}"
+                """
+            )
+        )
+        if test_role_arn:
             fp.write(
                 dedent(
                     f"""
-                    region = "{aws_region}"
+                    role_arn = "{test_role_arn}"
                     """
                 )
             )
-            if test_role_arn:
-                fp.write(
-                    dedent(
-                        f"""
-                        role_arn = "{test_role_arn}"
-                        """
-                    )
-                )
-        with terraform_apply(
-            terraform_module_dir,
-            destroy_after=not keep_after,
-            json_output=True,
-            enable_trace=TRACE_TERRAFORM,
-        ) as tf_out:
-            pprint(tf_out)
-
-    except CalledProcessError as err:
-        LOG.error(err)
-        LOG.info("STDOUT: %s", err.stdout)
-        LOG.error("STDERR: %s", err.stderr)
-        if TRACE_TERRAFORM:
-            LOG.info("Check output in files tf-apply-trace.txt, tf-destroy-trace.txt.")
-        sys.exit(1)
+    with terraform_apply(
+        terraform_module_dir,
+        destroy_after=not keep_after,
+        json_output=True,
+    ) as tf_out:
+        pprint(tf_out)
